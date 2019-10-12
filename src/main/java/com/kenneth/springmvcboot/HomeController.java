1d28309193a8fdac0ea2b98b6aebbb945cb92150
wuyeelok <wuyeelok@gmail.com>
2019-10-12 10:55:38 +0800

    Experi: Use model instead of modelandview

--- src/main/java/com/kenneth/springmvcboot/HomeController.java
+++ src/main/java/com/kenneth/springmvcboot/HomeController.java
@@ -1,10 +1,10 @@
 package com.kenneth.springmvcboot;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -15,15 +15,12 @@ public class HomeController {
 	}
 
 	@PostMapping("add")
-	public ModelAndView add(@RequestParam("num1") int i, @RequestParam("num2") int j) {
-
-		ModelAndView mv = new ModelAndView();
-		mv.setViewName("result");
+	public String add(@RequestParam("num1") int i, @RequestParam("num2") int j, Model m) {
 
 		int num3 = i + j;
-		mv.addObject("num3", num3);
+		m.addAttribute("num3", num3);
 
-		return mv;
+		return "result";
 	}
 
 }
