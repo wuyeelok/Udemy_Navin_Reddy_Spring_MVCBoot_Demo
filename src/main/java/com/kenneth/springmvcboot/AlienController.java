c7c364d6ca73a3378b8d37dc6bf8521d24e7933b
wuyeelok <wuyeelok@gmail.com>
2019-10-20 10:46:13 +0800

    Spring PathVariable: Simple single json return

--- src/main/java/com/kenneth/springmvcboot/AlienController.java
+++ src/main/java/com/kenneth/springmvcboot/AlienController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.kenneth.springmvcboot.model.Alien;
@@ -26,14 +27,23 @@ public class AlienController {
 	@ResponseBody
 	public List<Alien> getAliens(Model m) {
 
-		List<Alien> aliens = new ArrayList<>();
-
 		List<ReddyAlien> reddyAliens = this.repo.findAll();
 
-		for (ReddyAlien r : reddyAliens) {
-			aliens.add(r);
-		}
+		/*
+		 * for (ReddyAlien r : reddyAliens) { aliens.add(r); }
+		 */
+
+		List<Alien> aliens = new ArrayList<>(reddyAliens);
 
 		return aliens;
+	}
+
+	@GetMapping("alien/{aid}")
+	@ResponseBody
+	public Alien getAlien(@PathVariable("aid") int aid) {
+
+		Alien a = this.repo.findById(aid).orElse(new ReddyAlien(0, ""));
+
+		return a;
 	}
 }
