28ea8cfb05c96bc922f7edbc19e2b203a876ba10
wuyeelok <wuyeelok@gmail.com>
2019-10-20 12:10:18 +0800

    Spring REST: Define accept format only json

--- src/main/java/com/kenneth/springmvcboot/AlienController.java
+++ src/main/java/com/kenneth/springmvcboot/AlienController.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kenneth.springmvcboot.model.Alien;
@@ -45,8 +47,8 @@ public class AlienController {
 		return a;
 	}
 
-	@PostMapping("alien")
-	public Alien addAlien(Alien alien) {
+	@PostMapping(path = "alien", consumes = { MediaType.APPLICATION_JSON_VALUE })
+	public Alien addAlien(@RequestBody ReddyAlien alien) {
 
 		ReddyAlien ra = new ReddyAlien(alien.getAid(), alien.getAname());
 
