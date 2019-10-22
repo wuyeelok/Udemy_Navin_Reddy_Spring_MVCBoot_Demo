f891b2e5da0453a55b0318a8eadf0a89d64ccde6
wuyeelok <wuyeelok@gmail.com>
2019-10-22 10:14:32 +0800

    Spring AOP: Add logging before calling REST aliens

--- src/main/java/com/kenneth/springmvcboot/LoggingAspect.java
+++ src/main/java/com/kenneth/springmvcboot/LoggingAspect.java
@@ -1,9 +1,16 @@
 package com.kenneth.springmvcboot;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class LoggingAspect {
 
+	@Before("execution(public java.util.List<com.kenneth.springmvcboot.model.Alien> com.kenneth.springmvcboot.AlienController.getAliens(..))")
 	public void log() {
-		System.out.println("getAliens method called");
+		System.out.println("getAliens method called from aspect");
 	}
 
 }
